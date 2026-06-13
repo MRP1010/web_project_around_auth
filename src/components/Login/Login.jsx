9fc874eb6c7d914fbc06d24a638fbf256643a8f6
MRP1010 <mreynosos1@gmail.com>
2026-06-13 04:37:23 -0600

    P18 Ok Style

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { Link } from "react-router-dom";
+
 function Login({ onLogin }) {
   const [formData, setFormData] = useState({ email: "", password: "" });
 
@@ -15,7 +17,7 @@ function Login({ onLogin }) {
 
   return (
     <div className="auth">
-      <p className="auth__title">Iniciar sesión</p>
+      <p className="auth__title">Inicia sesión</p>
       <form onSubmit={handleSubmit} className="auth__form">
         <input
           required
@@ -36,9 +38,16 @@ function Login({ onLogin }) {
           className="auth__input"
         />
         <button type="submit" className="auth__button">
-          Iniciar sesión
+          Inicia sesión
         </button>
       </form>
+
+      <p className="auth__signup-prompt">
+        ¿Aún no eres miembro?{" "}
+        <Link to="/signup" className="auth__link">
+          Regístrate aquí
+        </Link>
+      </p>
     </div>
   );
 }
